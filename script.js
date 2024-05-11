1fe3afff5f80f88257f5613630f942f73605dfa6
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-05-11 12:56:17 +0200

    changeable background colors

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function myFunction() {
     var paragraphs = document.getElementsByTagName("p"); // Pobranie wszystkich elementów <p>
     for (var i = 0; i < paragraphs.length; i++) {
         paragraphs[i].style.color = "black"; // Zmiana koloru tekstu na czarny dla wszystkich elementów <p>
+        paragraphs[i].style.backgroundColor = "white";
     }
     var headings = document.getElementsByTagName("h3"); // Pobranie wszystkich elementów <h3>
     for (var i = 0; i < headings.length; i++) {
@@ -44,17 +45,6 @@ function myFunction() {
 }
 
 function myFunction2() {
-    document.body.style.backgroundColor = "white"; // Zmiana koloru tła body na białe
-    var paragraphs = document.getElementsByTagName("p"); // Pobranie wszystkich elementów <p>
-    for (var i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].style.color = "black"; // Zmiana koloru tekstu na czarny dla wszystkich elementów <p>
-    }
-    var headings = document.getElementsByTagName("h3"); // Pobranie wszystkich elementów <h3>
-    for (var i = 0; i < headings.length; i++) {
-        headings[i].style.color = "black"; // Zmiana koloru tekstu na czarny dla wszystkich elementów <h3>
-    }
-    var aboutDiv = document.querySelector(".git"); // Pobranie diva o klasie "about"
-    if (aboutDiv) {
-        aboutDiv.style.backgroundColor = "white"; // Zmiana koloru tła diva o klasie "about" na białe
-    }
+    location.reload();
 }
+

c4fa18f373a8abba1480b6bd65e05834d199be10
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-09-11 18:34:56 +0200

    Change color

--- src/js/script.js
+++ src/js/script.js
@@ -15,35 +15,93 @@ document.addEventListener("DOMContentLoaded", function () {
     handleScroll();
 });
 
-/*function myFunction() {
-    document.body.style.backgroundColor = "white"; 
-    var paragraphs = document.getElementsByTagName("p"); 
-    for (var i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].style.color = "black"; 
-        paragraphs[i].style.backgroundColor = "white";
+function changeColor() {
+    var currentMode = localStorage.getItem('colorMode');
+    
+    if (currentMode === 'default') {
+        localStorage.setItem('colorMode', 'changed');
+        localStorage.setItem('bodyColor', 'white');
+        localStorage.setItem('textColor', 'black');
+        localStorage.setItem('elementColor', 'white');
+    } else {
+        localStorage.setItem('colorMode', 'default');
+        localStorage.setItem('bodyColor', '');
+        localStorage.setItem('textColor', '');
+        localStorage.setItem('elementColor', '');
     }
-    var headings = document.getElementsByTagName("h3"); 
-    for (var i = 0; i < headings.length; i++) {
-        headings[i].style.color = "black"; 
-    }
-    var aboutDiv = document.querySelector(".about"); 
-    if (aboutDiv) {
-        aboutDiv.style.backgroundColor = "white"; 
-    }
+    
+    applyColors();
+}
 
-    var aboutDiv = document.querySelector(".git"); 
-    if (aboutDiv) {
-        aboutDiv.style.backgroundColor = "white"; 
-    }
+function applyColors() {
+    var bodyColor = localStorage.getItem('bodyColor');
+    var textColor = localStorage.getItem('textColor');
+    var elementColor = localStorage.getItem('elementColor');
 
+    if (bodyColor && textColor && elementColor) {
+        document.body.style.backgroundColor = bodyColor;
 
-    var aboutDiv = document.querySelector(".programming");
-    if (aboutDiv) {
-        aboutDiv.style.backgroundColor = "white"; 
+        var paragraphs = document.getElementsByTagName("p");
+        for (var i = 0; i < paragraphs.length; i++) {
+            paragraphs[i].style.color = textColor;
+            paragraphs[i].style.backgroundColor = elementColor;
+        }
+
+        var headings = document.getElementsByTagName("h3");
+        for (var i = 0; i < headings.length; i++) {
+            headings[i].style.color = textColor;
+        }
+
+        var aboutDiv = document.querySelector(".about");
+        if (aboutDiv) {
+            aboutDiv.style.backgroundColor = elementColor;
+        }
+
+        var gitDiv = document.querySelector(".git");
+        if (gitDiv) {
+            gitDiv.style.backgroundColor = elementColor;
+        }
+
+        var programmingDiv = document.querySelector(".programming");
+        if (programmingDiv) {
+            programmingDiv.style.backgroundColor = elementColor;
+        }
+    } else {
+        document.body.style.backgroundColor = '';
+        var paragraphs = document.getElementsByTagName("p");
+        for (var i = 0; i < paragraphs.length; i++) {
+            paragraphs[i].style.color = '';
+            paragraphs[i].style.backgroundColor = '';
+        }
+
+        var headings = document.getElementsByTagName("h3");
+        for (var i = 0; i < headings.length; i++) {
+            headings[i].style.color = '';
+        }
+
+        var aboutDiv = document.querySelector(".about");
+        if (aboutDiv) {
+            aboutDiv.style.backgroundColor = '';
+        }
+
+        var gitDiv = document.querySelector(".git");
+        if (gitDiv) {
+            gitDiv.style.backgroundColor = '';
+        }
+
+        var programmingDiv = document.querySelector(".programming");
+        if (programmingDiv) {
+            programmingDiv.style.backgroundColor = '';
+        }
     }
 }
 
-function myFunction2() {
-    location.reload();
-}*/
+window.onload = function() {
+    if (!localStorage.getItem('colorMode')) {
+        localStorage.setItem('colorMode', 'default');
+    }
+    applyColors();
+};
+
+
 
